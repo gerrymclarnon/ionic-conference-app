129ead6cc7caa408b598082088bdba0ddcb2914c
Gerry McLarnon <Gerry.McLarnon@tescobank.com>
2018-01-10 17:59:37 +0000

    Add Player model

---
+++ src/models/Player.ts
@@ -0,0 +1,12 @@
+export class Player {
+  key: string;
+  title: string;
+  email: string;
+
+  constructor(key: string, title: string, email: string) {
+    this.key = key;
+    this.title = title;
+    this.email = email;
+  }
+
+}
--- src/providers/list-service.ts
+++ src/providers/list-service.ts
@@ -1,6 +1,7 @@
 import {Injectable, NgZone} from '@angular/core';
 import firebase from 'firebase';
 import {Observable} from 'rxjs/Observable';
+import {Player} from '../models/Player';
 
 @Injectable()
 export class ListService {
@@ -8,16 +9,16 @@ export class ListService {
   constructor(public zone: NgZone) {
   }
 
-  getList(): Observable<any> {
+  getList(): Observable<Player> {
 
     return Observable.create((observer: any) => {
       firebase.database().ref("/list")
         .on("value", (snapshot: any) => {
-          let list: Array<any> = [];
-          snapshot.forEach((listItem: any) => {
-            let listItemToPush = listItem.val();
-            listItemToPush.key = listItem.key;
-            list.push(listItemToPush);
+          let list: Array<Player> = [];
+          snapshot.forEach((player: Player) => {
+            // let listItemToPush = listItem.val();
+            // listItemToPush.key = listItem.key;
+            list.push(new Player(player.key, player.val().title, player.val().email));
           });
           this.zone.run(() => {
             observer.next(list);
