546b6e859bfc7f160e703e15f16d166d7ac58997
Gerry McLarnon <gerry.mclarnon@tescobank.com>
2018-11-15 19:57:03 +0000

    Refactored to Ionic 4

---
+++ src/app/credentials.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB8Qx4nlrKBectiq1PxdMonO-IKyGP3lpA",
+  authDomain: "email-8734c.firebaseapp.com",
+  databaseURL: "https://email-8734c.firebaseio.com",
+  projectId: "email-8734c",
+  storageBucket: "email-8734c.appspot.com",
+  messagingSenderId: "476597523348"
+};
